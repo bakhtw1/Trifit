4dd52471dc68622b9a7453eca55f1416c9cd7882
Justin <justinjchester@icloud.com>
2022-02-17 02:20:54 -0500

    Add home page components and basic dummy data

---
+++ lib/assets/SampleData.dart
@@ -0,0 +1,74 @@
+List sampleHomeData = [
+  {
+    "SummaryCardData": {
+      "CaloriesFromFood": 1450,
+      "CaloriesFromExercise": 250,
+    },
+    "MealEntriesData": [
+      {
+        "type": "Breakfast",
+        "items": [
+          {
+            "name": "Banana",
+            "calories": 105,
+          },
+          {
+            "name": "Yogurt",
+            "calories": 118
+          },
+                    {
+            "name": "Banana",
+            "calories": 105,
+          },
+          {
+            "name": "Yogurt",
+            "calories": 118
+          },
+                    {
+            "name": "Banana",
+            "calories": 105,
+          },
+          {
+            "name": "Yogurt",
+            "calories": 118
+          }
+        ]
+      },
+      {
+        "type": "Lunch",
+        "items": [
+          {
+            "name": "Chicken",
+            "calories": 250,
+          },
+          {
+            "name": "Rice",
+            "calories": 200
+          }
+        ]
+      },
+      {
+        "type": "Snack",
+        "items": [
+          {
+            "name": "Oreos",
+            "calories": 300,
+          }
+        ]
+      },
+      {
+        "type": "Dinner",
+        "items": [
+          {
+            "name": "Ground Beef",
+            "calories": 400,
+          },
+          {
+            "name": "Potatoes",
+            "calories": 250
+          }
+        ]
+      }
+    ]
+  }
+];
