5f93697f743f4d3b9458836c9f43cd1e996a441e
Justin <justinjchester@icloud.com>
2022-02-16 22:15:43 -0500

    Add custom style

---
+++ lib/assets/Styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/*
+  This file can hold all of the custom colors / themes that we plan on using throughout the app (text styles, etc.)
+*/
+Map<int, Color> trifitColor =
+  {
+    50:Color.fromRGBO (165, 69, 204, .1),
+    100:Color.fromRGBO(165, 69, 204, .2),
+    200:Color.fromRGBO(165, 69, 204, .3),
+    300:Color.fromRGBO(165, 69, 204, .4),
+    400:Color.fromRGBO(165, 69, 204, .5),
+    500:Color.fromRGBO(165, 69, 204, .6),
+    600:Color.fromRGBO(165, 69, 204, .7),
+    700:Color.fromRGBO(165, 69, 204, .8),
+    800:Color.fromRGBO(165, 69, 204, .9),
+    900:Color.fromRGBO(165, 69, 204, 1),
+  };
