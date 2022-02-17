8bb2a6b9173ea995a51aed40ca28f96ee5bcd15e
Justin <justinjchester@icloud.com>
2022-02-17 02:20:40 -0500

    Add home page components and basic dummy data

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -1,4 +1,8 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
+import '../assets/Styles.dart' as tfStyle;
+import '../assets/SampleData.dart' as SampleData;
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,15 +12,134 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List mealData = SampleData.sampleHomeData[0]["MealEntriesData"];
+  List<Widget> mealSummaries = [];
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(widget.pageTitle),
+    for (var meal in mealData) {
+      mealSummaries.add(mealSummary(meal));
+    }
+    // This is so that there's white space at the bottom of the list so the last items are easily viewable. 140 is the default height of a card
+    mealSummaries.add(SizedBox(height: 140));
+    return  Stack(
+        children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              calorieSummary(),
+              //SizedBox(height: 20),
+              Expanded(child:ListView(
+                shrinkWrap: true,
+                children: mealSummaries
+                )
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 10,
+            right: 10,
+            child: FloatingActionButton(
+              backgroundColor: tfStyle.trifitColor[700],
+              onPressed: showAddFoodItemModal,
+              tooltip: 'Add a new entry',
+              child: const Icon(Icons.add),
+            ),
+          )
+        ],
+    );
+  }
+
+  /*
+    This will bring up the screen for adding a food item entry to the home page
+    For now, it will do nothing but print to console
+  */
+  void showAddFoodItemModal() {
+    print("Add food item");
+  }
+
+  Widget calorieSummary() {
+    List sampleHomeData = SampleData.sampleHomeData;
+    /*
+    Eventually these will all be computed properties or fetched, but for now it'll be dummy data
+    */
+    String calsFromFood = sampleHomeData[0]["SummaryCardData"]["CaloriesFromFood"].toString();
+    String calsFromExercise = sampleHomeData[0]["SummaryCardData"]["CaloriesFromExercise"].toString();
+    String netCals = (sampleHomeData[0]["SummaryCardData"]["CaloriesFromFood"] - sampleHomeData[0]["SummaryCardData"]["CaloriesFromExercise"]).toString();
+
+    return Container(
+      height: 140,
+      padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+      child: SizedBox.expand(
+        child: Card (
+          color: Color(0xFFEEEEEE),
+          shape: RoundedRectangleBorder(
+           // side: BorderSide(color: tfStyle.trifitColor[900]!, width: 2),
+            borderRadius: BorderRadius.circular(5)
+          ),
+          child: Padding(padding: EdgeInsets.all(5),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Calories", style: TextStyle(fontSize: 26)),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+                    child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(children: [Text(calsFromFood, style: tfStyle.homeCardTitleTextStyle), Text("Food")]),
+                      Column(children: [Text(calsFromExercise, style: tfStyle.homeCardTitleTextStyle), Text("Exercise")]),
+                      Column(children: [Text(netCals, style: tfStyle.homeCardTitleTextStyle) ,Text("Net")]),
+                    ],
+                  )
+                )
+              ],
+            )
+          )
+        )
+      )
+    );
+  }
+
+  Widget mealSummary(LinkedHashMap<String, Object> meal) {
+    List<Widget> items = [];
+    for (var item in meal["items"] as List) {
+      items.add(
+        Padding(
+        padding: EdgeInsets.only(top: 5, bottom: 5),
+        child: Row(
+          children: [
+            Text(item["name"]),
+            Spacer(),
+            Text(item["calories"].toString())
           ],
-        ),
+        )
+        )
       );
+    }
+    return Container(
+      // This manages to maintain proper heights without using expanded
+      height: 140 + (items.length - 2)*26,
+      padding: EdgeInsets.only(left: 10, right: 10, top: 10),
+      child: SizedBox.expand(
+        child: Card (
+          color: Color(0xFFEEEEEE),
+          shape: RoundedRectangleBorder(
+           // side: BorderSide(color: tfStyle.trifitColor[900]!, width: 2),
+            borderRadius: BorderRadius.circular(5)
+          ),
+          child: Padding(padding: EdgeInsets.only(left: 15, right: 15, top: 5, bottom: 5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text(meal["type"] as String, style: TextStyle(fontSize: 26)),
+                  SizedBox(height: 20),
+                  Column(children: items)
+                ],
+              
+            )
+          )
+        )
+      )
+    );
   }
 }
--- lib/assets/Styles.dart
+++ lib/assets/Styles.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 /*
   This file can hold all of the custom colors / themes that we plan on using throughout the app (text styles, etc.)
 */
+
+/*
+  Trifit logo color from our m1 presentation, with opacities from 10% to 100% (50 and 900 respectively)
+
+  If you run into the error 'The argument type 'Color?' can't be assigned to the parameter type 'Color' ' while
+  using this (e.g. trifitColor[900]), end it with a ! to force unwrap the optional value (e.g. trifitColor[900]!)
+*/
 Map<int, Color> trifitColor =
   {
     50:Color.fromRGBO (165, 69, 204, .1),
@@ -16,3 +23,7 @@ Map<int, Color> trifitColor =
     800:Color.fromRGBO(165, 69, 204, .9),
     900:Color.fromRGBO(165, 69, 204, 1),
   };
+
+  TextStyle homeCardTitleTextStyle = TextStyle(
+    fontSize: 24,
+  );
