74aaa95fe16aa482afe6a79f69b3fafedb474e4e
Waqas Bakht <bakhtw1@gmail.com>
2022-03-29 13:41:36 -0400

    Fixed issues with multidex, firebase init on android

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trifit/utilities/firebase_options.dart';
 import 'Screens/login.dart';
 import 'Screens/mainScreen.dart';
 import 'utilities/Styles.dart';
@@ -11,7 +12,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
