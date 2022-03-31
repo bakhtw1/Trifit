94ded6c2a0633b66d3b60e0882ad61e8947f5570
Justin <justinjchester@icloud.com>
2022-03-31 01:56:22 -0400

    remove local file writing stuff

--- lib/utilities/UtilityFunctions.dart
+++ lib/utilities/UtilityFunctions.dart
@@ -1,12 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'FileReadWrite.dart';
-
-writeJson(String filename, String content) async {
-    var file = FileReadWrite(filename);
-    await file.write(content);
-}
-
 DateTime simpleDate(DateTime date) {
   return new DateTime(date.year, date.month, date.day);
 }
