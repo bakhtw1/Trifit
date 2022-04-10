aaa7d25adf40df85b564ec023256d74232b50ca0
Ananya Thukral <46412697+AnanyaThukral@users.noreply.github.com>
2022-04-10 01:56:11 -0400

    writing feed data to firebase

--- lib/components/addfeedItem.dart
+++ lib/components/addfeedItem.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:trifit/models/FeedModel.dart';
+
+import '../controllers/feedController.dart';
 
 class AddFeedItem extends StatefulWidget {
   AddFeedItem({Key? key}) : super(key: key);
@@ -10,9 +14,11 @@ class AddFeedItem extends StatefulWidget {
 }
 
 class _AddFeedItemState extends State<AddFeedItem> {
-  final myController = TextEditingController();
+  var myController = TextEditingController();
+  var imgController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
-  CollectionReference feed = FirebaseFirestore.instance.collection("feed");
+  // CollectionReference feeds = FirebaseFirestore.instance.collection("feeds");
 
   @override
   void initState() {
@@ -24,56 +30,106 @@ class _AddFeedItemState extends State<AddFeedItem> {
     // Clean up the controller when the widget is removed from the widget tree.
     // This also removes the _printLatestValue listener.
     myController.dispose();
+    imgController.dispose();
     super.dispose();
   }
 
+  var feedController = FeedController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Create Post')),
-      body: Padding(
-        padding: EdgeInsets.all(25),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        "https://thumbs.dreamstime.com/b/portrait-young-african-american-business-woman-black-peop-people-51712509.jpg")),
-                SizedBox(
-                  width: 10,
+    return StreamBuilder<Object>(
+        stream: FirebaseFirestore.instance
+            .collection('meals')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return CircularProgressIndicator();
+          }
+          return Scaffold(
+            appBar: AppBar(title: Text('Create Post')),
+            body: Padding(
+              padding: EdgeInsets.all(25),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                "https://thumbs.dreamstime.com/b/portrait-young-african-american-business-woman-black-peop-people-51712509.jpg")),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'John Doe',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    // TextField(
+                    //   style: TextStyle(decoration: TextDecoration.none),
+                    //   decoration: InputDecoration(
+                    //     hintText: 'What would you like to share?',
+                    //     border: InputBorder.none,
+                    //     focusedBorder: InputBorder.none,
+                    //     enabledBorder: InputBorder.none,
+                    //   ),
+                    //   controller: myController,
+                    // ),
+
+                    TextFormField(
+                      controller: myController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'What would you like to share?',
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter some description for the post';
+                        }
+                        return null;
+                      },
+                    ),
+
+                    TextField(
+                      controller: imgController,
+                      decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        labelText: 'Enter image URL',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    ElevatedButton(
+                        // onPressed: () async {
+                        //   await feeds.add({'description': myController.text}).then(
+                        //       (value) => print('feed added'));
+                        // },
+                        onPressed: () async {
+                          if (_formKey.currentState!.validate()) {
+                            await feedController.addFeed(FeedModel(
+                                myController.text, imgController.text));
+
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                  content: Text('Successfully added post')),
+                            );
+                          }
+                        },
+                        child: Text('Share'))
+                  ],
                 ),
-                Text(
-                  'John Doe',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            TextField(
-              style: TextStyle(decoration: TextDecoration.none),
-              decoration: InputDecoration(
-                hintText: 'What would you like to share?',
-                border: InputBorder.none,
-                focusedBorder: InputBorder.none,
-                enabledBorder: InputBorder.none,
               ),
-              controller: myController,
             ),
-            ElevatedButton(
-                onPressed: () async {
-                  await feed.add({
-                    'name': 'ananya',
-                    'age': 23,
-                    'note': 'test feed'
-                  }).then((value) => print('feed added'));
-                },
-                child: Text('Share'))
-          ],
-        ),
-      ),
-    );
+          );
+        });
   }
 }
---
+++ lib/controllers/feedController.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:trifit/models/FeedModel.dart';
+import '../models/MealModel.dart';
+import '../utilities/UtilityFunctions.dart';
+
+class FeedController {
+  late StreamSubscription<DocumentSnapshot<Map<String, dynamic>>>
+      feedSubscription;
+  var allFeeds = [];
+
+  FeedController() {
+    feedSubscription = FirebaseFirestore.instance
+        .collection('feeds')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .snapshots()
+        .listen((snapshot) {
+      var data = snapshot.data();
+      if (data != null) {
+        allFeeds = data['feeds'];
+      }
+    });
+  }
+
+  addFeed(FeedModel toAdd) async {
+    FirebaseFirestore.instance
+        .collection('feeds')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .update({
+      "feeds": FieldValue.arrayUnion([toAdd.toJson()])
+    });
+  }
+}
